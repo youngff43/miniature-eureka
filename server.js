748b7eea78eb8f88e6d1bcaf5c4623a3729f3c44
youngff43 <youngff43@gmail.com>
2022-05-26 17:17:05 -0400

    Added Heroku

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+// required  
 const express = require('express');
+const PORT = process.env.PORT || 3001; 
 const app = express();
-const { notes } = require('./db/db');
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
   });
 
 
-  
+
  //http://localhost:3001/api/notes
